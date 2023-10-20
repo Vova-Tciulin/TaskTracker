a7db8a4910f35aaad08200b0f27f68625a78e6fd
vtciu <vtciulin@mail.ru>
2023-10-20 10:11:37 +0300

    add unversioned files

---
+++ src/services/TasksServices/Tasks.Cmd/Tasks.Cmd.Api/Program.cs
@@ -0,0 +1,38 @@
+using Tasks.Cmd.Api.Extensions;
+using Tasks.Cmd.Application;
+using Tasks.Cmd.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add Layers
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+//Add Automapper
+builder.Services.AddAutoMapper(typeof(Program));
+
+//Add Middleware
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
+
+var app = builder.Build();
+
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
